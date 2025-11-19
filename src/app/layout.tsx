499d53eff895241b99bdb9fa35fb0ff714a8a757
Eugene Uguomore <uguomoreeugene123@gmail.com>
2025-11-19 07:12:21 -0700

    Added GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // <--- Added this import
 import "./globals.css";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
@@ -19,6 +20,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* --- Start Google Tag Manager (The Brain) --- */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MR6MGMFT');
+          `}
+        </Script>
+        {/* --- End Google Tag Manager --- */}
+
+        {/* --- Start GTM NoScript (The Backup) --- */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MR6MGMFT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* --- End GTM NoScript --- */}
+
         <Navbar />
         {children}
         <Footer />

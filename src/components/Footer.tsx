080ef75e577274e1d1717081ac814da6c04a3a49
Eugene Uguomore <uguomoreeugene123@gmail.com>
2025-12-27 08:06:48 -0700

    Updated footer email to contact@propcoupouns.com

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,8 +35,8 @@ export const Footer = () => {
             </p>
             <div className="flex items-center gap-2 text-gray-400">
               <Mail className="w-4 h-4" />
-              <a href="mailto:support@propdiscounts.com" className="hover:text-white transition-colors">
-                support@propdiscounts.com
+              <a href="mailto:contact@propcoupouns.com" className="hover:text-white transition-colors">
+                contact@propcoupouns.com
               </a>
             </div>
           </div>
